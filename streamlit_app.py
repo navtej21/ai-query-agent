d9a1c9d36fa7249ab75b7a1cb44b8d688b2ef03b
Navtej S Nair <154542472+navtej21@users.noreply.github.com>
2025-01-05 10:56:35 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,11 +3,6 @@ from openai import OpenAI
 
 # Show title and description.
 st.title("📄 Document question answering")
-st.write(
-    "Upload a document below and ask a question about it – GPT will answer! "
-    "To use this app, you need to provide an OpenAI API key, which you can get [here](https://platform.openai.com/account/api-keys). "
-)
-
 # Ask user for their OpenAI API key via `st.text_input`.
 # Alternatively, you can store the API key in `./.streamlit/secrets.toml` and access it
 # via `st.secrets`, see https://docs.streamlit.io/develop/concepts/connections/secrets-management
